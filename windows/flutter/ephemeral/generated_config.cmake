632eb927aff3a0c029dbb029f47713c1c522daff
Erroradit <115888749+Erroradit@users.noreply.github.com>
2025-04-23 23:33:17 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Flutter SDK\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter projects\\to_do_supabase" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Flutter SDK\\src\\flutter"
+  "PROJECT_DIR=D:\\flutter projects\\to_do_supabase"
+  "FLUTTER_ROOT=E:\\Flutter SDK\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter projects\\to_do_supabase\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter projects\\to_do_supabase"
+  "FLUTTER_TARGET=D:\\flutter projects\\to_do_supabase\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter projects\\to_do_supabase\\.dart_tool\\package_config.json"
+)
